0779ea2328f87af6bf0d86b84ded1efadd54ec87
OSS-jy <ju010320@pukyong.ac.kr>
2024-03-13 16:20:49 +0900

    page not found 수정
    혹시 이거 떄문인가 싶어서....

--- test/myapi/urls.py
+++ test/myapi/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 #from course.views import course_list
 	
 urlpatterns = [
+    path('', admin.site.urls),
     path('admin/', admin.site.urls),
     path('user/', include('login.urls')), #login앱
     path('profile/', include('userinfo.urls')), #userinfo앱
